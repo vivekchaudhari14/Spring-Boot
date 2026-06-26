5e1688bf675cf9110484fd090a46116f30ea5433
vivekchaudhari14 <vivekchaudhari14@zohomail.in>
2026-06-26 19:07:42 +0530

    Create Static Dataset

--- bookrestbook/src/main/java/com/springboot/api/book/controller/BookController.java
+++ bookrestbook/src/main/java/com/springboot/api/book/controller/BookController.java
@@ -1,10 +1,16 @@
 package com.springboot.api.book.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.api.book.services.BookService;
+
 @RestController
 public class BookController {
+	
+	@Autowired
+	private BookService bookService;
 	
 	@GetMapping("/books")
 	public String getAllBookHelper(){
--- bookrestbook/src/main/java/com/springboot/api/book/services/BookService.java
+++ bookrestbook/src/main/java/com/springboot/api/book/services/BookService.java
@@ -1,5 +1,27 @@
 package com.springboot.api.book.services;
 
-public class BookService {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+import com.springboot.api.book.entities.Book;
+
+@Component
+public class BookService {
+	private static List<Book> list = new ArrayList<>();
+	
+	static {
+		list.add(new Book(1,"java","james gosling"));
+		list.add(new Book(1,"java","james gosling"));
+		list.add(new Book(1,"java","james gosling"));
+		list.add(new Book(1,"java","james gosling"));
+		
+	}
+	
+	// get All Student 
+	
+	public List<Book> getAllBook(){
+		return list;
+	}
 }
