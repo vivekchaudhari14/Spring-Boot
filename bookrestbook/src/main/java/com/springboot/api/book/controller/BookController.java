e8f6f78671e200de908487a77b04eb5536612462
vivekchaudhari14 <vivekchaudhari14@zohomail.in>
2026-06-26 18:52:22 +0530

    Controller Testing

--- bookrestbook/src/main/java/com/springboot/api/book/controller/BookController.java
+++ bookrestbook/src/main/java/com/springboot/api/book/controller/BookController.java
@@ -1,5 +1,13 @@
 package com.springboot.api.book.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookController {
 	
+	@GetMapping("/books")
+	public String getAllBookHelper(){
+		return "This is helper Method";
+	}
 }
