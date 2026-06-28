9dbabf93ffe92882b4f21c1ada6b3d65f9ac8b73
vivekchaudhari14 <vivekchaudhari14@zohomail.in>
2026-06-28 16:48:30 +0530

    Initial commit thymeleaf

---
+++ thymeleaf/src/main/java/com/springboot/thymeleaf/practice/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.springboot.thymeleaf.practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping(value = "/about" ,method= RequestMethod.GET)
+	public String about() {
+		System.out.println("About Handler");
+		return "about";	}	
+}
