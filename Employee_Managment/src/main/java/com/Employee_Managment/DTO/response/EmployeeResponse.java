b5a19771318c764055dd1f30aabc0293f2b12dc9
vivekchaudhari14 <vivekchaudhari14@zohomail.in>
2026-07-22 17:56:10 +0530

    Add employeeResponseDto

---
+++ Employee_Managment/src/main/java/com/Employee_Managment/DTO/response/EmployeeResponse.java
@@ -0,0 +1,38 @@
+package com.Employee_Managment.DTO.response;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import com.Employee_Managment.enump.Gender;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class EmployeeResponse {
+	
+     Long id;
+     String employeeCode;
+     String firstName;
+     String surName;
+     String email;
+     String mobileNumber;
+     Gender gender;
+     LocalDate dob;
+     LocalDate joiningDate;
+     BigDecimal salary;
+     Long departmentId;
+     Long designationId;
+     Boolean status;
+     Instant createdAt;
+     Instant updatedAt;
+}
